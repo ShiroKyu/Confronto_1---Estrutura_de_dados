0926f3aa9d3f33365b485396d7b8103deeada9da
Paulo <paulo.leite@academico.ifpb.edu.br>
2019-12-24 17:54:53 -0300

    Definindo TAD

--- Confronto1.h
+++ Confronto1.h
@@ -14,5 +14,18 @@
 /* Criar TAD para a fila de processos */
 
 typedef struct fila Fila;
+typedef struct nodo Nodo;
+
+struct nodo {
+	int id;    //ID do processo
+	int tempo; //Tempo de espera em segundo
+}
+
+struct fila {
+	Nodo *inicio;
+	Nodo *fim;
+};
+
+
 
 ///////////////////////////////
