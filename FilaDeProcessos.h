e0c70fc2cf8c1d468ea70cb8f8d7f9152a950f05
Paulo <paulo.leite@academico.ifpb.edu.br>
2020-01-05 22:01:28 -0300

    Modificacoes 03 05/01/2020

---
+++ FilaDeProcessos.h
@@ -0,0 +1,40 @@
+//Cabeçalho de funções e protótipos
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+
+/* Protótipo das funções da fila de processos - Paulo*/
+
+/* Criar TAD para a fila de processos */
+
+typedef struct fila Fila;
+typedef struct processo Processo;
+
+
+
+//Criar a fila de processos
+Fila *criaFila();
+
+/* Verifica se a fila de processos está vazia */
+int vazio(Fila *f);
+
+/* função auxiliar: insere no fim */
+Processo* insere_fim (Processo* fim, int id);
+
+// Função que insere
+void insere (Fila* f, int id);
+
+// Função que retira o processo com maior tempo de espera
+void retiraProcesso(Fila *f);
+
+//Imprime os processos da fila
+void imprimirProcessos(Fila *f);
+
+// Liberar a fila
+void liberar(Fila *f);
+
+///////////////////////////////
